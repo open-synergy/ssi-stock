cd1289f8049c2db5edb544c336ab0f867417b0f6
Andhitia Rama <andhitia.r@gmail.com>
2023-05-24 06:31:48 +0700

    [ssi_stock] 14.0.1.0.0

---
+++ ssi_stock/__manifest__.py
@@ -0,0 +1,33 @@
+# Copyright 2022 OpenSynergy Indonesia
+# Copyright 2022 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+{
+    "name": "Inventory",
+    "version": "14.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "LGPL-3",
+    "installable": True,
+    "application": True,
+    "depends": [
+        "ssi_master_data_mixin",
+        "ssi_product",
+        "stock",
+    ],
+    "data": [
+        "security/ir_module_category_data.xml",
+        "security/res_group_data.xml",
+        "security/ir.model.access.csv",
+        "data/stock_picking_type_category_data.xml",
+        "data/location_type_data.xml",
+        "menu.xml",
+        "wizards/mass_create_location_views.xml",
+        "wizards/mass_create_picking_type_views.xml",
+        "views/picking_type_category_views.xml",
+        "views/stock_picking_type_views.xml",
+        "views/stock_picking_views.xml",
+        "views/location_type_views.xml",
+        "views/stock_location_views.xml",
+    ],
+    "demo": [],
+}
---
+++ ssi_stock/models/__init__.py
@@ -0,0 +1,12 @@
+# Copyright 2023 OpenSynergy Indonesia
+# Copyright 2023 PT. Simetri Sinergi Indonesia
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+from . import (
+    picking_type_category,
+    stock_picking,
+    stock_picking_type,
+    location_type,
+    stock_location,
+    stock_move,
+)
