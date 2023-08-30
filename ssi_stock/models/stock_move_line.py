e7a1b5dfeabed9a899ed4bd132c310732cb2a578
Andhitia Rama <andhitia.r@gmail.com>
2023-08-30 19:55:15 +0700

    [UPD] ssi_stock
    * Fix auto generate lot algo
    * Hide some fields

--- ssi_stock/models/stock_move_line.py
+++ ssi_stock/models/stock_move_line.py
@@ -19,15 +19,17 @@ class StockMoveLine(models.Model):
         if self.lot_name or self.lot_id:
             return True
 
-        if self.product_id.categ_id.sequence_id:
-            sequence = self.product_id.categ_id.sequence_id
-            if self.move_id.date_backdating:
-                ctx = {"ir_sequence_date": self.move_id.date_backdating}
-                sequence = sequence.with_context(ctx).next_by_id()
-            else:
-                sequence = sequence.next_by_id()
-            self.write(
-                {
-                    "lot_name": sequence,
-                }
-            )
+        if not self.product_id.categ_id.sequence_id:
+            return True
+
+        sequence = self.product_id.categ_id.sequence_id
+        if self.move_id.date_backdating:
+            ctx = {"ir_sequence_date": self.move_id.date_backdating}
+            number = sequence.with_context(ctx).next_by_id()
+        else:
+            number = sequence.next_by_id()
+        self.write(
+            {
+                "lot_name": number,
+            }
+        )
