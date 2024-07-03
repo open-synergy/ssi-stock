332948dbb42c0fd7f004b794c1f489de5ffac199
Andhitia Rama <andhitia.r@gmail.com>
2024-07-03 10:26:35 +0700

    [UPD] ssi_stock
    * QC HT/24/000640

--- ssi_stock/models/stock_production_lot.py
+++ ssi_stock/models/stock_production_lot.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 
-from odoo import fields, models, api
+from odoo import api, fields, models
 
 
 class StockProductionLot(models.Model):
@@ -11,24 +11,24 @@ class StockProductionLot(models.Model):
     _inherit = ["stock.production.lot"]
 
     @api.depends(
+        "stock_move_line_ids",
         "stock_move_line_ids.date",
         "stock_move_line_ids.move_id.state",
-        "tracking"
+        "tracking",
     )
     def _compute_serial_number_in_date(self):
         for rec in self:
             serial_number_in_date = False
             if rec.tracking == "serial":
-                first_move_line_id = self.env["stock.move.line"].search([
-                    ("id", "=", rec.stock_move_line_ids.ids),
-                ], order="date", limit=1)
+                criteria = [("lot_id", "=", rec.id), ("state", "=", "done")]
+
+                first_move_line_id = self.env["stock.move.line"].search(
+                    criteria, order="date, id", limit=1
+                )
                 serial_number_in_date = first_move_line_id.date
             rec.serial_number_in_date = serial_number_in_date
 
-    tracking = fields.Selection(
-        related="product_id.tracking",
-        store=True
-    )
+    tracking = fields.Selection(related="product_id.tracking", store=True)
     stock_move_line_ids = fields.One2many(
         comodel_name="stock.move.line",
         inverse_name="lot_id",
